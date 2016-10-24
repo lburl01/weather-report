face3c8dce1d5f61224d97de9b7165a3c5ac1a26
Lori Burleson <lori.burleson@gmail.com>
2016-10-24 15:46:06 -0400

    Caching theoretically still working and pulling from conditions api

--- weather.rb
+++ weather.rb
@@ -3,6 +3,8 @@ require_relative 'cache'
 
 @cache = Cache.new
 
+city_state = ARGV
+
 def get_json(url)
   return @cache.get(url) if @cache.key?(url)
 
@@ -12,6 +14,7 @@ def get_json(url)
   return data
 end
 
-data = get_json('http://api.wunderground.com/api/7d88bd8046a20136/conditions/q/NC/Raleigh.json')
+data = get_json("http://api.wunderground.com/api/7d88bd8046a20136/conditions/q/#{city_state[1]}/#{city_state[0]}.json")
 
-p data
+puts "The weather for #{city_state[0]} #{city_state[1]}: "
+puts "It's " + data['current_observation']['weather'] + " and feels like: " + data['current_observation']['feelslike_string'] + "."
