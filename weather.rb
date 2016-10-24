624efe95b5d76484332406838c96f7433222d43c
Lori Burleson <lori.burleson@gmail.com>
2016-10-24 16:21:50 -0400

    Printing ten day forecast

--- weather.rb
+++ weather.rb
@@ -15,7 +15,7 @@ def get_json(url)
   return data
 end
 
-data = get_json("http://api.wunderground.com/api/7d88bd8046a20136/conditions/astronomy/alerts/q/#{city_state[1]}/#{city_state[0]}.json")
+data = get_json("http://api.wunderground.com/api/7d88bd8046a20136/conditions/alerts/astronomy/forecast10day/q/#{city_state[1]}/#{city_state[0]}.json")
 
 if data['alerts'] == []
   data['alerts'] = 0
@@ -26,4 +26,9 @@ puts "It's " + data['current_observation']['weather'] + " and feels like: " + da
 print "Sunrise is at " + data['sun_phase']['sunrise']['hour'] + ":" + data['sun_phase']['sunrise']['minute'] + "AM."
 puts " Sunset is at " + data['sun_phase']['sunset']['hour'] + ":" + data['sun_phase']['sunset']['minute'] + "PM."
 puts "Current weather alerts: #{data['alerts']}"
-# p data
+puts "Ten Day Forecast:"
+puts data['forecast']['txt_forecast']['forecastday'][0]['title'] + ": " + data['forecast']['txt_forecast']['forecastday'][0]['fcttext']
+puts data['forecast']['txt_forecast']['forecastday'][2]['title'] + ": " + data['forecast']['txt_forecast']['forecastday'][2]['fcttext']
+puts data['forecast']['txt_forecast']['forecastday'][4]['title'] + ": " + data['forecast']['txt_forecast']['forecastday'][4]['fcttext']
+puts data['forecast']['txt_forecast']['forecastday'][6]['title'] + ": " + data['forecast']['txt_forecast']['forecastday'][6]['fcttext']
+puts data['forecast']['txt_forecast']['forecastday'][8]['title'] + ": " + data['forecast']['txt_forecast']['forecastday'][8]['fcttext']
