23cb8dc48e470c668c3bd2a56c2106da7211e1d1
Lori Burleson <lori.burleson@gmail.com>
2016-10-24 16:08:41 -0400

    Successfully printing conditions and astronomy apis with one call

--- weather.rb
+++ weather.rb
@@ -1,5 +1,6 @@
 require 'httparty'
 require_relative 'cache'
+require 'json'
 
 @cache = Cache.new
 
@@ -14,7 +15,10 @@ def get_json(url)
   return data
 end
 
-data = get_json("http://api.wunderground.com/api/7d88bd8046a20136/conditions/q/#{city_state[1]}/#{city_state[0]}.json")
+data = get_json("http://api.wunderground.com/api/7d88bd8046a20136/conditions/astronomy/q/#{city_state[1]}/#{city_state[0]}.json")
 
-puts "The weather for #{city_state[0]} #{city_state[1]}: "
+puts "All about weather for #{city_state[0]} #{city_state[1]}: "
 puts "It's " + data['current_observation']['weather'] + " and feels like: " + data['current_observation']['feelslike_string'] + "."
+print "Sunrise is at " + data['sun_phase']['sunrise']['hour'] + ":" + data['sun_phase']['sunrise']['minute'] + "AM."
+puts " Sunset is at " + data['sun_phase']['sunset']['hour'] + ":" + data['sun_phase']['sunset']['minute'] + "PM."
+# p data
